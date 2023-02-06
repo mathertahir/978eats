58c6541d4fb287aa10000e58ddbc990a08f04525
Mather <mather@pixarsart.com>
2023-02-06 23:17:29 +0500

    Animation Complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Topnavs from './components/Topnavs'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import Upperbanner from './components/Upperbanner';
+import Bikeanimation from './components/Bikeanimation';
 
 
 const App = () => {
   return (
     <div><Topnavs/>
     <Upperbanner/>
+   <Bikeanimation/>
     </div>
   
   )
---
+++ src/components/Bikeanimation.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import "../CSS/bikeanimation.css"
+
+const Bikeanimation = () => {
+  return (
+    <>
+
+<section>
+    <div className='road-size'>
+
+    <img src='images/road.png' className='road-image'></img>
+</div>
+<div className='moving-section'>
+    <img src='images/bikelogo.svg'  className='bike-image  card-img-overlay' ></img>
+</div>
+
+</section>
+
+
+
+    </>
+  )
+}
+
+export default Bikeanimation
